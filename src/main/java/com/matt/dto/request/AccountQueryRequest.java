535276dfa5257c25d3dbea1c2ab35d1eb4de764c
matt zi yang <newyork1771021@126.com>
2026-04-20 15:35:58 +0800

    fix: remove redudant xml

--- src/main/java/com/matt/dto/request/AccountQueryRequest.java
+++ src/main/java/com/matt/dto/request/AccountQueryRequest.java
@@ -3,6 +3,9 @@ package com.matt.dto.request;
 import lombok.Data;
 import java.math.BigDecimal;
 
+/**
+ * Query parameters are optional. Null values will be ignored in the query.
+ */
 @Data
 public class AccountQueryRequest {
     private Long creatorId;
